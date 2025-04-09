2a7dada0ffa7dc7fbaf5b659aadd07654563d511
jucabrera <jucabrera@unal.edu.co>
2025-04-09 08:32:21 -0500

    Version_5,Ejercicio

--- Juego-Programa.cpp
+++ Juego-Programa.cpp
@@ -14,6 +14,8 @@ Informar si lo adivino o no
     Este valor de 10 permanecerá inalterable durante toda la ejecución del programa. Ese numero no se puede cambiar con nada,
      es constante y entero,*/
     int guessed_numbrer;
+    
+    for ( ; ; ) {
     std::cout <<"Adivina un numero entre 1 y 100:\n"; // Mostrar el comentario
     std::cin >> guessed_numbrer; // Determina la variable que declare, Cin es herramienta de lectura
 
@@ -21,7 +23,8 @@ Informar si lo adivino o no
     /* Determina que si sudece una cosa, esta lo hara
     == Igual, != Desigualdad, < menor, > mayor, <= menor igual, >= mayor igual, ** Elevar un numero
     */
-        std::cout <<"GANASTE!!!\n";  
+        std::cout <<"GANASTE!!!\n";
+        break;  
         }
     else if (guessed_numbrer > NUM) {
         /* Es cuando la respuesta noes la pedida, por lo tanto se considera falsa*/
@@ -29,6 +32,7 @@ Informar si lo adivino o no
         }
     else { // es el menor
         std::cout << "Te falta\n";
+    }
     }
 
     return 0;
